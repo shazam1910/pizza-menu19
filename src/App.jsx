6b5113c1673f339526aa5fa973222907f37ea103
Shashank Pratheepkumar <Shashanksanthosh19@gmail.com>
2024-06-26 15:19:05 +0530

    Updating pizza items in menu

--- src/App.jsx
+++ src/App.jsx
@@ -72,20 +72,47 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <Pizza 
-      name="Pizza Spinaci"
-      ingredients="Tomato, mozarella, spinach, and ricotta cheese"
-      photoName="pizzas/spinaci.jpg"
-      price={12}
+      <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={12}
       />
 
-      <Pizza 
-      nname="Pizza Funghi"
-      ingredients="Tomato, mozarella, mushrooms, and onion"
-      photoName="pizzas/funghi.jpg"
-      price={12}/>
-      <Pizza />
-      <Pizza />
+      <Pizza
+        name="Pizza Funghi"
+        ingredients="Tomato, mozarella, mushrooms, and onion"
+        photoName="pizzas/funghi.jpg"
+        price={12}
+      />
+
+      <Pizza
+        name="Focaccia"
+        ingredients="Bread with italian olive oil and rosemary"
+        photoName="pizzas/focaccia.jpg"
+        price={6}
+      />
+
+      <Pizza
+        name="Pizza Margherita"
+        ingredients="Tomato and mozarella"
+        photoName="pizzas/margherita.jpg"
+        price={10}
+      />
+
+      <Pizza
+        name="Pizza Salamino"
+        ingredients="Tomato, mozarella, and pepperoni"
+        photoName="pizzas/salamino.jpg"
+        price={15}
+      />
+
+      <Pizza
+        name="Pizza Prosciutto"
+        ingredients="Tomato, mozarella, ham, aragula, and burrata cheese"
+        photoName="pizzas/prosciutto.jpg"
+        price={18}
+      />
     </main>
   );
 }
@@ -114,8 +141,6 @@ function Footer() {
     </footer>
   );
 }
-
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
