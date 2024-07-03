40ba8a7e934ad15e13a638a9c544dbf46a441941
Shashank Pratheepkumar <Shashanksanthosh19@gmail.com>
2024-07-03 12:08:45 +0530

    Using conditional rendering to open time and pizza items

--- src/App.jsx
+++ src/App.jsx
@@ -69,15 +69,20 @@ function Header() {
 }
 
 function Menu() {
+  const pizzas = pizzaData;
+  const numPizzas = pizzas.length;
+
   return (
     <main className="menu">
       <h2>Our Menu</h2>
 
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => (
-          <Pizza pizzaObj={pizza} key={pizza.name} />
-        ))}
-      </ul>
+      {numPizzas > 0 && (
+        <ul className="pizzas">
+          {pizzaData.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      )}
 
       {/* <Pizza
         name="Pizza Spinaci"
@@ -144,7 +149,12 @@ function Footer() {
 
   return (
     <footer classNamer="footer">
-      {new Date().toLocaleTimeString()}. We are currently open
+      <div className="order">
+        {isOpen && (
+          <p>We are open until {closeHour}:00, Come visit us or order online</p>
+        )}
+      </div>
+      <button className="btn">Order</button>
     </footer>
   );
 }
