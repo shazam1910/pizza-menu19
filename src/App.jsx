ebfc469a3bcdae0a2bbd4786f086dee87a0f17e0
Shashank Pratheepkumar <Shashanksanthosh19@gmail.com>
2024-07-03 14:28:30 +0530

    Setting up the sold out feature

--- src/App.jsx
+++ src/App.jsx
@@ -135,14 +135,14 @@ function Menu() {
 }
 
 function Pizza({pizzaObj}) {
-  if (pizzaObj.soldOut) return null;
+  // if (pizzaObj.soldOut) return null;
 
   return (
-    <li>
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <h3>{pizzaObj.name}</h3>
       <p>{pizzaObj.ingredients}</p>
-      <span>{pizzaObj.price}</span>
+      <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
     </li>
   );
 }
