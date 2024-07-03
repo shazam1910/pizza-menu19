ce5d2f2ab3f92f436643f8f0124b62401cc31117
Shashank Pratheepkumar <Shashanksanthosh19@gmail.com>
2024-07-03 14:04:20 +0530

    Add fragment for menu message

--- src/App.jsx
+++ src/App.jsx
@@ -77,11 +77,14 @@ function Menu() {
       <h2>Our Menu</h2>
 
       {numPizzas > 0 ? (
+        <>
+        <p>Authentiv Italian cuisine. 6 creative dishes to choose from. All from our stone oven, all organic, all delicious. </p>
         <ul className="pizzas">
           {pizzaData.map((pizza) => (
             <Pizza pizzaObj={pizza} key={pizza.name} />
           ))}
         </ul>
+          </>
       ) : (
         <p>We are still working on our menu. Please comeback later :)</p>
       )}
