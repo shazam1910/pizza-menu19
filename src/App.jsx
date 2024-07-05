d7ecffd99d32f462598772acd3cd65150a5ab0be
Shashank Pratheepkumar <Shashanksanthosh19@gmail.com>
2024-07-05 16:06:22 +0530

    Minor bug fixing

--- src/App.jsx
+++ src/App.jsx
@@ -140,9 +140,12 @@ function Pizza({pizzaObj}) {
   return (
     <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
+      <div>
+
       <h3>{pizzaObj.name}</h3>
       <p>{pizzaObj.ingredients}</p>
       <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
+      </div>
     </li>
   );
 }
